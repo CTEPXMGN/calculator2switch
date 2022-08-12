67b123e7854df97906d1882c57710f5bc4d53d47
CTEPXMGN <fuser-mgn@ya.ru>
2022-08-12 09:17:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function calc(operator, a, b) {
+
+    switch (operator) {
+        case 'add':
+            return a + b;
+            break;
+        case 'multi':
+            return a * b;
+            break;
+        case 'subtract':
+            return a - b;
+            break;
+        default:
+            break;
+    }
+}
+
+console.log(calc('add', 2, 5));
+console.log(calc('multi', 2, 5));
+console.log(calc('subtract', 2, 5));
